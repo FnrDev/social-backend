5dcc01355eeca7e345294c749c8797d351a02a2c
FnrDev <fnr@pbtax.xyz>
2023-12-15 13:41:04 +0300

    add name to validation schema

--- src/validations/login.ts
+++ src/validations/login.ts
@@ -2,5 +2,6 @@ import { z } from 'zod';
 
 export const LoginSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(6)
+    password: z.string().min(6),
+    name: z.string().min(1).max(30)
 })
