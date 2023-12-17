79ac14b664278a8b7fc3745a2ff50f379da643b0
FnrDev <fnr@pbtax.xyz>
2023-12-17 19:40:33 +0300

    imporve auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -8,26 +8,26 @@ export default async function auth(req: Request, res: Response, next: NextFuncti
     // get the session from redis cache first
     const cachedSession: string | null = await redis.get(`session_${req.headers.authorization}`);
 
-    // session was not found in redis cache, find it in database
-    if (!cachedSession) {
-        const session = await database.session.findUnique({
-            where: { id: req.headers.authorization },
-            include: {
-                user: true
-            }
-        });
-
-        // session not found in database also
-        if (!session) return res.status(401).json({ error: true, message: "Wrong authorization" });
-        
-        // store the session in redis cache also
-        await redis.set(`session_${req.headers.authorization}`, JSON.stringify(session), { ex: 3600 });
-        // store session in express locals
-        res.locals.session = session;
-        return next();
-    } else {
-        // session found in redis
+    // session found in redis
+    if (cachedSession) {
         res.locals.session = JSON.parse(cachedSession);
-        next();
+        return next();
     }
+
+    // find session in database
+    const session = await database.session.findUnique({
+        where: { id: req.headers.authorization },
+        include: {
+            user: true
+        }
+    });
+
+    // session not found in database also
+    if (!session) return res.status(401).json({ error: true, message: "Wrong authorization" });
+    
+    // session founded in database store in redis cache
+    await redis.set(`session_${req.headers.authorization}`, JSON.stringify(session), { ex: 3600 });
+    // store session in express locals
+    res.locals.session = session;
+    return next();
 }
