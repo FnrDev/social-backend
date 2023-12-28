6f6bebeb798b50fe83f937520340fa8eb538754e
FnrDev <fnr5541@gmail.com>
2023-12-28 11:07:28 +0300

    fix: return required keys only

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth';
+import { User } from '@prisma/client';
 export const UserRouter = Router();
 
 UserRouter.get("/@me", auth, async (req, res) => {
-    return res.json(res.locals.session.user)
+    // @ts-ignore
+    const user: User = res.locals;
+    return res.json({
+        email: user.email,
+        createdAt: user.createdAt,
+        id: user.id
+    })
 })
